a31dfbe033bd8fb3f2663d6e6001e3e5e38ee666
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2024-05-01 21:16:10 +0530

    Conditions

---
+++ 02_ConditionalsLoops/conditions.java
@@ -0,0 +1,17 @@
+package com.nilam;
+
+public class conditions {
+    public static void main(String[] args) {
+        /*
+        if(condition){
+        //body
+        }
+        else if(condition){
+        //body
+        }
+        else{
+        //instructions
+        }
+         */
+    }
+}
