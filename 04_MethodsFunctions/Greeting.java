8778dc0277dbcab778570df81d2011588e5da904
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2024-05-04 22:36:14 +0530

    function greeting

---
+++ 04_MethodsFunctions/Greeting.java
@@ -0,0 +1,10 @@
+package com.nilam;
+
+public class Greeting {
+    public static void main(String[] args) {
+        greeting();  //return type void h toh bas function call kiye
+    }
+    static void greeting(){
+        System.out.println("Hello World");
+    }
+}
