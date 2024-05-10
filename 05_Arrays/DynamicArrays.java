cf49bfd83d4debcee29b7dca45154411e13d4322
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2024-05-10 22:36:39 +0530

    user input

---
+++ 05_Arrays/DynamicArrays.java
@@ -0,0 +1,10 @@
+package nilam;
+
+import java.util.Scanner;
+
+public class DynamicArrays {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+            
+    }
+}
