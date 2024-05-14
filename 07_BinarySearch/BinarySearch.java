487c1e81948981cb1d7925a71915e344c1f2aa10
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2024-05-14 18:10:19 +0530

    binary search

---
+++ 07_BinarySearch/BinarySearch.java
@@ -0,0 +1,30 @@
+package nilam;
+
+public class BinarySearch {
+    //it works for sorted arrays
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 9, 12, 43, 54, 67};
+        int target = 43;
+        int ans = binarySearch(arr,target);
+        System.out.println(ans);
+    }
+
+    static int binarySearch(int[] arr,int target){
+        int start = 0 ;
+        int end = arr.length - 1;
+
+        while(start <= end){
+//            int mid = (start + end)/2; // this may exceed range of int
+            int mid = start + (end - start)/2 ;
+
+            if(target > arr[mid]){
+                start = mid + 1;
+            }else if(target < arr[mid]){
+                end = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+        return - 1;
+    }
+}
