e76235bd51152e11ad4ef516051b725af87688aa
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2024-05-16 22:35:53 +0530

    num smaller or equal

---
+++ 07_BinarySearch/FloorOfNumber.java
@@ -0,0 +1,33 @@
+package nilam;
+
+public class FloorOfNumber {
+    // return index of number smaller than or equal to target
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 14, 16, 18, 46};
+        int target = 15;
+        int ans = floor(arr, target);
+        System.out.println(ans);
+    }
+
+    static int floor(int[] arr, int target){
+        //if target is smaller than the smallest number
+        if(target < arr[0]){
+            return -1;
+        }
+        int start = 0 ;
+        int end = arr.length - 1;
+
+        while(start <= end){
+             int mid = start + (end - start)/2 ;
+
+             if(target > arr[mid]){
+                 start = mid + 1;
+             }else if (target < arr[mid]){
+                 end = mid - 1;
+             }else{
+                 return mid;
+             }
+        }
+        return end;
+    }
+}
