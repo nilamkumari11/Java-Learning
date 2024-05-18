4626627814d6235b5ee3514bdfcbb58060c21a2e
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2024-05-18 21:59:55 +0530

    Roation Count

---
+++ 07_BinarySearch/RotationCount.java
@@ -0,0 +1,36 @@
+package nilam;
+
+public class RotationCount {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(rotationCount(arr));
+    }
+
+    static int rotationCount(int[] arr){
+        int pivot = findPivot(arr);
+        return pivot+1;
+    }
+
+    static int findPivot(int[] arr){
+        int start = 0 ;
+        int end = arr.length-1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            if(arr[mid] < arr[mid-1]){
+                return mid-1;
+            }
+            if(arr[mid] > arr[start]){
+                start = mid+1;
+            }
+            if(arr[mid] <= arr[end]){
+                end = mid-1;
+            }
+        }
+
+        return -1;
+    }
+}
