b0b150d8a1bd7e89a8a4eeed2fbd96bff118a7f8
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2024-07-25 20:25:19 +0530

    Strings

---
+++ 09_Strings/Strings.java
@@ -0,0 +1,11 @@
+package nilam;
+
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Nilam";
+        System.out.println(name);
+        name = "Kumari";  //String is immutable but here we are creating a new object thats why!!
+        System.out.println(name);
+
+    }
+}
