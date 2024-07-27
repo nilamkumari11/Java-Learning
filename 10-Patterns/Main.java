3758a94d35f0296ddf05686d36f104bc034bc2b4
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2024-07-27 19:30:03 +0530

    patterns

---
+++ 10-Patterns/Main.java
@@ -0,0 +1,72 @@
+package nilam;
+
+public class Main {
+    public static void main(String[] args) {
+        pattern31(4);
+    }
+
+    static void patter1(int n){
+        for (int row = 1; row <=n ; row++) {
+            //for every row run the column
+            for (int col = 1; col <= n-row; col++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <=row ; col++) {
+                System.out.print("* ");
+            }
+            //when one is completed we'll add a nw line
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n){
+        for (int row = 0; row < 2*n; row++) {
+            int totalColsInRow = row>n ? 2*n-row: row;
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern28(int n){
+        for (int row = 0; row < 2*n; row++) {
+            int totalColsInRow = row>n ? 2*n-row: row;
+            int noOfSpaces = n - totalColsInRow;
+            for (int s = 0; s < noOfSpaces; s++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern30(int n){
+        for (int row = 1; row <= n ; row++) {
+            for (int s = 0; s < n-row; s++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col >=1 ; col--) {
+                System.out.print(col+" ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern31(int n){
+        int original_n = n;
+        n=2*n;
+        for (int row = 0; row <= n; row++) {
+            for (int col = 0; col <= n; col++) {
+                int atEveryIndex = original_n - Math.min(Math.min(row, col),Math.min(n-row, n-col));
+                System.out.print(atEveryIndex + " ");
+            }
+            System.out.println();
+        }
+    }
+}
