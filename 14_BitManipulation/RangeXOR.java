e19c1e75c14051921920d1bc0109c4841de227c6
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2024-08-23 22:37:53 +0530

    Xor in range

---
+++ 14_BitManipulation/RangeXOR.java
@@ -0,0 +1,31 @@
+public class RangeXOR {
+    public static void main(String[] args) {
+        // range XOR from a to b = XOR(b) ^ XOR(a-1)
+        int a =3, b =9;
+
+        int ans = xor(b)^xor(a-1);
+
+        System.out.println(ans);
+
+        //only for check, will give time limit exceed for larger values
+        int ans2=0;
+        for (int i = a; i <= b; i++) {
+            ans2 ^= i;
+        }
+        System.out.println(ans2);
+        }
+
+    //this gives XOR from 0 to a
+    static int xor(int a){
+        if(a % 4==0){
+            return a;
+        }
+        if(a % 4==1){
+            return 1;
+        }
+        if(a % 4==2){
+            return a+1;
+        }
+        return 0;
+    }
+}
