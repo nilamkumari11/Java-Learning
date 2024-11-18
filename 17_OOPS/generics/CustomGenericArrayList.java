94a60e2802afb2b54db1664760a7dcf23c3cb89d
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2024-11-18 21:54:47 +0530

    Custom array list

---
+++ 17_OOPS/generics/CustomGenericArrayList.java
@@ -0,0 +1,68 @@
+package generics;
+
+import java.util.Arrays;
+
+public class CustomGenericArrayList<T> {
+    // instance variables
+    private Object[] data;
+    private static int DEFAULT_SIZE= 10;
+    private int size = 0; // also working as index value
+
+    public CustomGenericArrayList() {
+        this.data = new Object[DEFAULT_SIZE];
+    }
+
+    public void add(T num) {
+        if(isFull()) {
+            resize();
+        }
+        data[size++] = num;
+    }
+
+    private void resize() {
+        Object[] temp = new Object[data.length * 2];
+
+        // copy the current items in the new array
+        for( int i = 0; i < data.length; i++) {
+            temp[i] = data[i];
+        }
+        data = temp;
+    }
+
+    public T remove() {
+        T removed = (T)data[--size];
+        return removed;
+    }
+
+    public T get(int index) {
+        return (T)data[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void set(int index, T value) {
+        data[index] = value;
+    }
+
+    private boolean isFull() {
+        return size == data.length;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomArrayList{" +
+                "data=" + Arrays.toString(data) +
+                ", size=" + size +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        CustomGenericArrayList<Integer> list = new CustomGenericArrayList<>();
+        for(int i = 0; i < 14; i++) {
+            list.add(i*2);
+        }
+        System.out.println(list);
+    }
+}
