e997517fdb2115747017ddba0ec4ae80364a79f7
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2024-11-21 21:06:54 +0530

    implements B

---
+++ 17_OOPS/interfaces/extendDemo/Main.java
@@ -0,0 +1,15 @@
+package interfaces.extendDemo;
+
+public class Main implements B{
+    // implement both the methods
+    // B extending A
+    @Override
+    public void greet() {
+
+    }
+
+    @Override
+    public void fun() {
+
+    }
+}
