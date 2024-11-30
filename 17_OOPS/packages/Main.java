52956a1e2d768ebe3b791d505c2977f4d26f92fc
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2024-11-30 21:51:16 +0530

    basics

---
+++ 17_OOPS/packages/Main.java
@@ -0,0 +1,13 @@
+package packages;
+
+public class Main {
+    public static void main(String[] args) {
+        // Packages are of 2 types -> user-defined & In-Built
+        // In Built -> 1. lang ( essential files of java, language specific stuff ) ( Automatically imported )
+        // 2. io (input output classes)
+        // 3. util ( utility classes -> data structure and stuff, collection framework )
+        // 4. applet
+        // 5. awt (GUI)
+        // 6. net
+    }
+}
