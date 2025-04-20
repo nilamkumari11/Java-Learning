9eafdacba72105d5b57961bc1489f34559f7f8a1
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2025-04-20 23:27:21 +0530

    Stacks and Queues

---
+++ 18_StackQueue/InbuiltExamples.java
@@ -0,0 +1,37 @@
+package com.nilam;
+
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class InbuiltExamples {
+    public static void main(String[] args) {
+//        Stack<Integer> stack = new Stack<>();
+//        //stack -> stack memory Stack -> eap memeory
+//        stack.push(37);
+//        stack.push(40);
+//        stack.push(10);
+//        stack.push(20);
+//
+//        System.out.println(stack.pop());
+
+//        Queue<Integer> queue = new LinkedList<>(); // Queue is an interface
+//        // functions are performed via linkedlist class
+//        queue.add(3);
+//        queue.add(5);
+//        queue.add(9);
+//
+//        System.out.println(queue.peek());
+//        System.out.println(queue.remove());
+
+        // when use stacks and queues
+        // when need to store result so far
+        // BFS DFS
+
+        //DEQUE -> Doubly ended queue (Deck)
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        // ArrayDeque class helps to create object of the methods in Deque
+
+
+    }
+}
