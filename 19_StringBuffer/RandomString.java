8f1eb5075ecf43b5de8a1d7c6e6880a7378c7f8a
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2025-07-20 21:39:59 +0530

    generate string

---
+++ 19_StringBuffer/RandomString.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class RandomString {
+    static String generate(int size){
+        StringBuffer sb = new StringBuffer(size);
+
+        Random random = new Random();
+        for(int i=0; i< size; i++) {
+            int randomChar = 97 + (int)(random.nextFloat() * 26);
+            sb.append((char)randomChar);
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        int n =20;
+        String name = RandomString.generate(n);
+        System.out.println(name);
+    }
+}
+
