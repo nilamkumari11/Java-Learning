aa2b8163efd7cada1303153bcdbaf9b83a706415
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2025-09-11 21:48:01 +0530

    practice

--- 22_Question/StackQuestion.java
+++ 22_Question/StackQuestion.java
@@ -1,7 +1,21 @@
+import java.util.Stack;
+
 public class StackQuestion {
     public static void main(String[] args) {
-
+        int pulse = pulseCount(new int[]{3,1,4,2,5,1});
+        System.out.println(pulse);
     }
 
-    
+    public static int pulseCount(int[] arr){
+        Stack<Integer> s = new Stack<>();
+        int c=0;
+        for(int i=0; i<arr.length; i++){
+            while(!s.isEmpty() && arr[i]>s.peek()){
+                s.pop();
+                c++;
+            }
+            s.push(arr[i]);
+        }
+        return c;
+    }
 }
